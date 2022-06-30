ff0a6f03ba26c6215b22d57e9354ba802d642913
Nikunj Saraf <nikunjsaraf2001@gmail.com>
2022-06-30 16:31:50 +0530

    Add marker

--- Assets/Scripts/PlaceOnPlane.cs
+++ Assets/Scripts/PlaceOnPlane.cs
@@ -1,85 +1,94 @@
-using System.Collections.Generic;
+﻿using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.Events;
 using UnityEngine.XR.ARFoundation;
 using UnityEngine.XR.ARSubsystems;
 
-/// <summary>
-/// Listens for touch events and performs an AR raycast from the screen touch point.
-/// AR raycasts will only hit detected trackables like feature points and planes.
-///
-/// If a raycast hits a trackable, the <see cref="PlacedPrefab"/> is instantiated
-/// and moved to the hit position.
-/// </summary>
-[RequireComponent(typeof(ARRaycastManager))]
-public class PlaceOnPlane : MonoBehaviour
-{
-    [SerializeField] [Tooltip("Instantiates this prefab on a plane at the touch location.")]
-    private GameObject mPlacedPrefab;
+    /// <summary>
+    /// Listens for touch events and performs an AR raycast from the screen touch point.
+    /// AR raycasts will only hit detected trackables like feature points and planes.
+    ///
+    /// If a raycast hits a trackable, the <see cref="placedPrefab"/> is instantiated
+    /// and moved to the hit position.
+    /// </summary>
+    [RequireComponent(typeof(ARRaycastManager))]
+    public class PlaceOnPlane : MonoBehaviour
+    {
+        [SerializeField]
+        [Tooltip("Instantiates this prefab on a plane at the touch location.")]
+        GameObject m_PlacedPrefab;
 
-    private UnityEvent _placementUpdate;
+    UnityEvent placementUpdate;
 
-    [SerializeField] private GameObject visualObject;
+    [SerializeField]
+    GameObject visualObject;
 
     /// <summary>
     /// The prefab to instantiate on touch.
     /// </summary>
-    public GameObject PlacedPrefab
-    {
-        get => mPlacedPrefab;
-        set => mPlacedPrefab = value;
-    }
-
-    /// <summary>
-    /// The object instantiated as a result of a successful raycast intersection with a plane.
-    /// </summary>
-    private GameObject SpawnedObject { get; set; }
-
-    private void Awake()
-    {
-        _mRaycastManager = GetComponent<ARRaycastManager>();
-
-        _placementUpdate ??= new UnityEvent();
-
-        _placementUpdate.AddListener(DisableVisual);
-    }
-
-    private static bool TryGetTouchPosition(out Vector2 touchPosition)
-    {
-        if (Input.touchCount > 0)
+    public GameObject placedPrefab
         {
-            touchPosition = Input.GetTouch(0).position;
-            return true;
+            get { return m_PlacedPrefab; }
+            set { m_PlacedPrefab = value; }
         }
 
-        touchPosition = default;
-        return false;
-    }
+        /// <summary>
+        /// The object instantiated as a result of a successful raycast intersection with a plane.
+        /// </summary>
+        public GameObject spawnedObject { get; private set; }
 
-    private void Update()
-    {
-        if (!TryGetTouchPosition(out var touchPosition))
-            return;
-
-        if (!_mRaycastManager.Raycast(touchPosition, SHits, TrackableType.PlaneWithinPolygon)) return;
-        // Raycast hits are sorted by distance, so the first one
-        // will be the closest hit.
-        var hitPose = SHits[0].pose;
-
-        if (SpawnedObject == null)
+        void Awake()
         {
-            SpawnedObject = Instantiate(mPlacedPrefab, hitPose.position, hitPose.rotation);
+            m_RaycastManager = GetComponent<ARRaycastManager>();
+
+            if (placementUpdate == null)
+                placementUpdate = new UnityEvent();
+
+                placementUpdate.AddListener(DiableVisual);
         }
 
-        _placementUpdate.Invoke();
-    }
+        bool TryGetTouchPosition(out Vector2 touchPosition)
+        {
+            if (Input.touchCount > 0)
+            {
+                touchPosition = Input.GetTouch(0).position;
+                return true;
+            }
 
-    private void DisableVisual()
+            touchPosition = default;
+            return false;
+        }
+
+        void Update()
+        {
+            if (!TryGetTouchPosition(out Vector2 touchPosition))
+                return;
+
+            if (m_RaycastManager.Raycast(touchPosition, s_Hits, TrackableType.PlaneWithinPolygon))
+            {
+                // Raycast hits are sorted by distance, so the first one
+                // will be the closest hit.
+                var hitPose = s_Hits[0].pose;
+
+                if (spawnedObject == null)
+                {
+                    spawnedObject = Instantiate(m_PlacedPrefab, hitPose.position, hitPose.rotation);
+                    
+                }
+                // else
+                // {
+                //     spawnedObject.transform.position = hitPose.position;
+                // }
+                    placementUpdate.Invoke();
+            }
+        }
+
+    public void DiableVisual()
     {
         visualObject.SetActive(false);
     }
 
-    private static readonly List<ARRaycastHit> SHits = new();
+        static List<ARRaycastHit> s_Hits = new List<ARRaycastHit>();
 
-    private ARRaycastManager _mRaycastManager;
-}
+        ARRaycastManager m_RaycastManager;
+    }
--- Assets/VIsual Object/Scripts/PlacementIndicator.cs
+++ Assets/VIsual Object/Scripts/PlacementIndicator.cs
@@ -1,39 +1,37 @@
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.XR.ARFoundation;
 using UnityEngine.XR.ARSubsystems;
-using static UnityEngine.Screen;
 
-namespace Visual_Object.Scripts
+public class PlacementIndicator : MonoBehaviour
 {
-    public class PlacementIndicator : MonoBehaviour
+
+    private ARRaycastManager rayManager;
+    [SerializeField]
+    private GameObject visual;
+
+    void Start()
     {
+        // get the components
+        rayManager = FindObjectOfType<ARRaycastManager>();
+        visual = transform.GetChild(0).gameObject;
 
-        private ARRaycastManager _rayManager;
-        [SerializeField]
-        private GameObject visual;
+        // hide the placement indicator visual
+        visual.SetActive(false);
+    }
 
-        private void Start()
+    void Update()
+    {
+        // shoot a raycast from the center of the screen
+        List<ARRaycastHit> hits = new List<ARRaycastHit>();
+        rayManager.Raycast(new Vector2(Screen.width / 2, Screen.height / 2), hits, TrackableType.Planes);
+
+        // if we hit an AR plane surface, update the position and rotation
+        if (hits.Count > 0)
         {
-            // get the components
-            _rayManager = FindObjectOfType<ARRaycastManager>();
-            visual = transform.GetChild(0).gameObject;
-
-            // hide the placement indicator visual
-            visual.SetActive(false);
-        }
-
-        private void Update()
-        {
-            // shoot a raycast from the center of the screen
-            var hits = new List<ARRaycastHit>();
-            _rayManager.Raycast(new Vector2(width / 2, height / 2), hits, TrackableType.Planes);
-
-            // if we hit an AR plane surface, update the position and rotation
-            if (hits.Count <= 0) return;
-            var transform1 = transform;
-            transform1.position = hits[0].pose.position;
-            transform1.rotation = hits[0].pose.rotation;
+            transform.position = hits[0].pose.position;
+            transform.rotation = hits[0].pose.rotation;
 
             // enable the visual if it's disabled
             if (!visual.activeInHierarchy)
